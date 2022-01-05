491fb6c1f10be3f9b3d7cef6c8238a5de466c4de
Aaron K <tonberryspawn@gmail.com>
2022-01-05 14:09:32 -0800

    W9D3 - ERB intro, Navigating Rails - pro tips

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    # raise @category.inspect # throw/raises an error (like a breakpoint)
     @products = @category.products.order(created_at: :desc)
   end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,18 @@
 class ProductsController < ApplicationController
 
   def index
+    # uses MODEL
     @products = Product.all.order(created_at: :desc)
+    # VIEW
+    # render :index # implicitly renders view template
   end
 
   def show
     @product = Product.find params[:id]
+    
+    @msg = "Hello"
+    # instance vars are magically transferred from controller to be available in all templates rendered as part of this request response
+    # render :show # can also use string for path, ex. 'shared/show'
   end
 
 end
