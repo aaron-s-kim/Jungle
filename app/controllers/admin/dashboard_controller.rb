45e1c6c778c914965c2a6af1dd558572ffb7d0f7
Aaron K <tonberryspawn@gmail.com>
2022-01-05 19:01:36 -0800

    W9D3 - Feature - Admin Dashboard - Counts - css update

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   def show
     @products = Product.all.order(created_at: :desc)
     @categories = Category.all.order(created_at: :desc)
+    @product_count = Product.all.order(created_at: :desc).count
+    @category_count = Category.all.order(created_at: :desc).count
   end
 end
