bfa99f3eb706161696eb0c7da36bda934cffd10e
Aaron K <tonberryspawn@gmail.com>
2022-01-06 20:58:32 -0800

    if any sale is active, display it on the products#index page and affect the prices - created sales_helper - added class method to check for active sale

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,13 @@
 class Sale < ActiveRecord::Base
 
+  
+  # AR Scope
+  def self.active # class method
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+  end
+  # Can also use this instead:
+  # scope :active, -> { where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current) }
+
   def finished?
     ends_on < Date.current # implied self.ends_on
   end
