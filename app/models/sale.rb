4eb4a1250282582e1794f5f62e0e360d67626c55
Aaron K <tonberryspawn@gmail.com>
2022-01-06 20:36:49 -0800

    added data into sales table, updated view to show sale records, and indicate active status

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,15 @@
 class Sale < ActiveRecord::Base
+
+  def finished?
+    ends_on < Date.current # implied self.ends_on
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
+
 end
