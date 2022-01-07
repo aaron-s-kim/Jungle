edde3146735b5573250818984ac6b19ee5768ffe
Aaron K <tonberryspawn@gmail.com>
2022-01-06 18:12:21 -0800

    - updated ApplicationController with methods to find user if logged-in, and save user object to variable - added before_filter to AboutController - updated layout/application to show login/signup links, and user's name if logged in

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,9 @@
 class AboutController < ApplicationController
+
+  # add this line to any controller you want secured
+  # will force user login before seeing actions in this controller
+  before_filter :authorize
+
   def index
   end
 end
